551e360a04706c53d6ca4371b964a7ebeb5a2870
sorongosdev <dosora9@gmail.com>
2024-09-11 04:39:11 +0900

    ms-phd candidates 수정중

---
+++ js/profile.js
@@ -0,0 +1,86 @@
+$(document).ready(function () {
+  // 프로필을 생성하는 함수
+  function loadProfiles(url, containerClass) {
+    $.getJSON(url).done(function (people) {
+      const container = $(containerClass);
+
+      people.forEach((person) => {
+        const profile = `
+          <div class="col-12 col-lg-4">
+            <!-- 프로필 이미지 -->
+            <img
+              class="portrait"
+              src="${person.profile_img}"
+              alt="${person.name}-profile-img"
+            />
+
+            <!-- 프로필 이미지 아래 설명 -->
+            <div class="portrait-title">
+              <h2>${person.name}</h2>
+              <h3>${person.position}</h3>
+
+              <!-- 프로필 아래 링크 아이콘 -->
+              <ul class="network-icon" aria-hidden="true">
+                ${
+                  person.google_scholar
+                    ? `
+                <li>
+                  <a href="${person.google_scholar}">
+                    <img src="img/google-scholar-svg.svg" />
+                  </a>
+                </li>
+                `
+                    : ""
+                }
+                ${
+                  person.cv
+                    ? `
+                <li>
+                  <a href="${person.cv}">
+                    <img src="img/cv-svg.svg" />
+                  </a>
+                </li>
+                `
+                    : ""
+                }
+                ${
+                  person.linkedin
+                    ? `
+                <li>
+                  <a href="${person.linkedin}">
+                    <img src="img/linkedin-svg.svg" />
+                  </a>
+                </li>
+                `
+                    : ""
+                }
+                ${
+                  person.orcid
+                    ? `
+                <li>
+                  <a href="${person.orcid}">
+                    <img src="img/orcid-svg.svg" />
+                  </a>
+                </li>
+                `
+                    : ""
+                }
+              </ul>
+            </div>
+          </div>
+        `;
+        container.append(profile);
+      });
+    });
+  }
+
+  // 교수님
+  loadProfiles(
+    "json/people/principal_investigator.json",
+    ".principal-investigator"
+  );
+  // Ph.D./M.S. Students
+  loadProfiles("json/people/phd_ms.json", ".phd-ms-students");
+  // M.S.-Ph.D. Candidates
+  loadProfiles("json/people/ms_phd_candidates.json", ".ms-phd-candidates");
+});
