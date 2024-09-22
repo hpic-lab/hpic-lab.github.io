38d187789285e4db0db5a823e6e31564d1bf10e6
sorongosdev <dosora9@gmail.com>
2024-09-23 03:22:37 +0900

    pub conference 완

--- js/publication.js
+++ js/publication.js
@@ -1,40 +1,32 @@
-{
-  /* <img class="pub-icon" src="img/pub-svg.svg" />
-<span class="pub-author">
-  <span>
-    ${}
-  </span>
-  <span>(연도)</span>
-</span> */
-}
-
 $(document).ready(function () {
   // 프로필을 생성하는 함수
-  function loadJournal(url, containerClass) {
-    $.getJSON(url).done(function (journals) {
+  function loadPublication(url, containerClass) {
+    $.getJSON(url).done(function (pubs) {
       const container = $(containerClass);
 
-      journals.forEach((journal) => {
-        const authorsList = journal.authors
+      pubs.forEach((pub) => {
+        const authorsList = pub.authors
           .map((author) => `<span>${author}</span>`)
           .join(", ");
-        const journal_detail = `
+
+        const publicationSource = pub.journal ? pub.journal : pub.conference;
+
+        const pub_detail = `
         <div class="pub-wrapper">
-          <span>
-            <span class="pub-icon-box"><img src="img/pub-svg.svg"></span>
-            <span class="pub-author">
-              ${authorsList}
-            </span>
-            <span> (${journal.year}).</span>
-            <span><b> ${journal.title}.</b></span>
-            <span> (${journal.journal}).</span>
+          <span class="pub-icon-box"><img src="img/pub-svg.svg"></span>
+          <span class="pub-author">
+            ${authorsList}
           </span>
+          <span> (${pub.year}).</span>
+          <span><a href="${pub.link}" target="_blank"><b> ${pub.title}.</b></a></span>
+          <span> (${publicationSource}).</span>
         </div>
         `;
-        container.append(journal_detail);
+        container.append(pub_detail);
       });
     });
   }
 
-  loadJournal("json/publications/journal.json", ".journal-container");
+  loadPublication("json/publications/journal.json", ".journal-container");
+  loadPublication("json/publications/conference.json", ".conference-container");
 });
