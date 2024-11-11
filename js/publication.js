ccb8ec75411a76140ff01578008e1e4da82518d7
hpic-lab <cms4429@gmail.com>
2024-11-11 19:58:36 +0900

    Update publication.js

--- js/publication.js
+++ js/publication.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
         <div class="pub-wrapper">
           <span class="pub-icon-box"><img src="img/pub-svg.svg"></span>
           <span class="badge text-bg-primary"> ${pub.type}</span>|
-          <span class="badge process-badge">${pub.status}</span>|
+          <span class="badge bg-success">${pub.status}</span>|
           <span class="pub-author">
             ${authorsList}
           </span>
