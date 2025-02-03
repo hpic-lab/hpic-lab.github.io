848e4287af56781a3c55ccd0b4c921b6c30224a0
hpic-lab <cms4429@gmail.com>
2025-02-03 13:14:52 +0900

    dd

--- js/publication.js
+++ js/publication.js
@@ -15,7 +15,8 @@ $(document).ready(function () {
         <div class="pub-wrapper">
           <span class="pub-icon-box"><img src="img/pub-svg.svg"></span>
           <span class="badge text-bg-primary"> ${pub.type}</span>|
-          <span class="badge bg-success">${pub.status}</span>
+          <span class="badge bg-success">${pub.status}</span>|
+          <span class="badge process-badge">${pub.registration}</span>
           <br>
           <span class="pub-author">
             ${authorsList}
