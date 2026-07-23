55a40a2c357f690c0546a5d9784afe54d792b389
cms4429 <cms4429@gmail.com>
2026-07-23 22:00:48 +0900

    style: Publications 탭 라벨 축약·글씨 축소, ECA 축소

--- js/publication.js
+++ js/publication.js
@@ -309,8 +309,8 @@ $(document).ready(function () {
       '<div class="pub2-side-nav">' +
         '<p class="pub2-notice">&dagger; Equally Credited Authors</p>' +
         '<div class="pub2-side-tabs">' +
-          '<button type="button" class="pub2-tab pub2-tab-journal active" data-target="journal">Journal Publication</button>' +
-          '<button type="button" class="pub2-tab pub2-tab-conference" data-target="conference">Conference Proceedings</button>' +
+          '<button type="button" class="pub2-tab pub2-tab-journal active" data-target="journal">Journal</button>' +
+          '<button type="button" class="pub2-tab pub2-tab-conference" data-target="conference">Conference</button>' +
           '<button type="button" class="pub2-tab pub2-tab-patent" data-target="patent">Patent</button>' +
         "</div>" +
         '<div class="pub2-year-links"></div>' +
