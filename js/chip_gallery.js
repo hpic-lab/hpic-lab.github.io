62ec79ff9a1cf582b093e60f9ada1267df5ee856
cms4429 <cms4429@gmail.com>
2026-07-30 10:17:33 +0900

    Fix mobile chip layout: force row break so image drops below title row

--- js/chip_gallery.js
+++ js/chip_gallery.js
@@ -243,6 +243,8 @@ $(document).ready(function () {
           // Failed 칩은 디자이너 아이콘 미표시 (추후 복원하려면 아래 조건 제거)
           (failed ? "" : '<div class="chip-title-desg">' + designerHTML(chip) + "</div>") +
         "</div>" +
+        // 모바일 전용 강제 줄바꿈: 위(연번+제목) / 아래(이미지+메타) 를 확실히 분리
+        '<div class="chip-rowbreak"></div>' +
         '<div class="chip-thumb' + (failed ? " chip-thumb-failed" : "") + '">' +
           (chip.image
             ? '<img src="' + chip.image + '" alt="' + (chip.name || "") + '" loading="lazy">'
